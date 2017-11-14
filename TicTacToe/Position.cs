660dacc5bb6cd4275625dad690749209feb26de1
Eze Kohon <ezekohon@gmail.com>
2017-11-14 16:59:05 -0300

    Add project files.

---
+++ TicTacToe/Position.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe {
+    class Position {
+        public int Row { get; }
+        public int Column { get; }
+        public Position(int row, int column) { Row = row; Column = column; }
+    }
+}
